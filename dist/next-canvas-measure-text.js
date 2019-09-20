ca18b89330a05779337df24e7d0ed4091204190e
afeiship <1290657123@qq.com>
2019-09-20 14:40:42 +0800

    add: callback

--- dist/next-canvas-measure-text.js
+++ dist/next-canvas-measure-text.js
@@ -2,18 +2,19 @@
  * name: next-canvas-measure-text
  * url: https://github.com/afeiship/next-canvas-measure-text
  * version: 1.0.0
- * date: 2019-09-20T05:59:32.016Z
+ * date: 2019-09-20T06:40:35.208Z
  * license: MIT
  */
 
 (function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
-  var DEFAULT_OPTIONS = { context: null, text: '' };
+  var DEFAULT_OPTIONS = { context: null, callback: nx.noop, text: '' };
 
   nx.canvasMeasureText = function(inOptions) {
     var options = nx.mix(null, DEFAULT_OPTIONS, inOptions);
     var ctx = options.context;
+    options.callback(ctx);
     return ctx.measureText(options.text);
   };
 
--- src/next-canvas-measure-text.js
+++ src/next-canvas-measure-text.js
@@ -1,11 +1,12 @@
 (function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
-  var DEFAULT_OPTIONS = { context: null, text: '' };
+  var DEFAULT_OPTIONS = { context: null, callback: nx.noop, text: '' };
 
   nx.canvasMeasureText = function(inOptions) {
     var options = nx.mix(null, DEFAULT_OPTIONS, inOptions);
     var ctx = options.context;
+    options.callback(ctx);
     return ctx.measureText(options.text);
   };
 
